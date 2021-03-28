878eeb4ad94ba8cb19a34edeffedfd7f6429639a
phantomoh <phantomoh98@gmail.com>
2021-03-28 20:09:39 +0900

    0328문제

---
+++ 0328.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 28 20:08:29 2021
+
+@author: USER
+"""
+def solution(s):
+    s = sorted(s,reverse=True)
+    answer = ''.join(s)
+    return answer
